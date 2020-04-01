f8979e7f39d9d2a51e9a4d0c8da9f7b1cf6e7775
Amant Arnaud <contact@arnaudamant.fr>
2020-04-01 21:28:36 +0200

    add documentation link

--- main.php
+++ main.php
@@ -4,6 +4,12 @@
  * @author Arnaud Amant <contact@arnaudamant.fr>
  */
 
+/**
+ * @see https://developers.scaleway.com/en/products/instance/api/#introduction
+ * @see https://www.scaleway.com/en/docs/how-to-backup-your-data/#-Deleting-a-Backup-via-the-Scaleway-API
+ * @see http://docs.guzzlephp.org/en/5.3/quickstart.html#
+ */
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
